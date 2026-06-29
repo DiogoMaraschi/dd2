7725ba0b27920b76620ad4b26112d642860918a7
DiogoMaraschi <maraschidiogo@gmail.com>
2026-06-29 14:50:00 -0300

    Meio de ligação - Dentro do Prazo

---
+++ d1.dart
@@ -0,0 +1,48 @@
+// NAO PODE SER MODIFICADO
+// -------------------------------------------------------------
+import 'dart:math' as math;
+
+void main() {
+  final meioDeComunicacao = aleatorio();
+  meioDeComunicacao.fazerLigacao('(47) 99876-5432');
+}
+
+MeioDeComunicacao aleatorio() {
+  final meiosDeComunicacao = <MeioDeComunicacao>[
+    Telefone(),
+    Celular(),
+    Orelhao(),
+  ];
+
+  final random = math.Random();
+
+  return meiosDeComunicacao[random.nextInt(meiosDeComunicacao.length)];
+}
+
+// -------------------------------------------------------------
+// ADICIONAR IMPLEMENTACAO RESTANTE ABAIXO DESSA LINHA
+// -------------------------------------------------------------
+abstract class MeioDeComunicacao {
+  fazerLigacao(String numero);
+}
+
+class Telefone extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print('[TELEFONE] Ligando para $numero...');
+  }
+}
+
+class Celular extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print('[CELULAR] Ligando para $numero...');
+  }
+}
+
+class Orelhao extends MeioDeComunicacao {
+  @override
+  void fazerLigacao(String numero) {
+    print('[ORELHÃO] Ligando para $numero...');
+  }
+}
